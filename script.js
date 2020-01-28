bf78e61e01697a7f480e0c83ce820d6b2d90c574
danghaonhien <58311274+danghaonhien@users.noreply.github.com>
2020-01-27 19:55:52 -0800

    add questions and answers

---
+++ script.js
@@ -0,0 +1,101 @@
+// Press Any Key to Start
+let start= false;
+let level = 0;
+let countdown=0;
+let myQuestions =[ {
+question: "Question 1?",
+options: ["a","b","c","d"],
+answer: "c"
+},{
+    question: "Question 2?",
+options: ["a","b","c","d"],
+answer: "c"
+},{
+    question: "Question 3?",
+options: ["a","b","c","d"],
+answer: "c"
+},{
+    question: "Question 4?",
+options: ["a","b","c","d"],
+answer: "c"
+},{
+    question: "Question 5?",
+options: ["a","b","c","d"],
+answer: "c"
+}
+]
+
+let chosenAnswer = [];
+
+
+
+$(document).keypress(function() {
+    if (!start) {
+   
+     
+    
+      start = true;
+     
+        
+    let timeLeft = 3;
+    let countdownTimer = setInterval(function() {
+        $("pressKey").value = timeLeft - 1;
+        $("#pressKey").html =$("#pressKey").text (timeLeft + " seconds remaining");
+        timeLeft -= 1;
+        if(timeLeft <= 0) {
+            clearInterval(countdownTimer);
+                 //High Score Timer
+            let highscore = 100;
+            let highscoreTimer = setInterval(function() {
+                $("#highScore").value = highscore - 1;
+                $("#highScore").html =$("#highScore").text (highscore + " seconds remaining");
+                highscore -= 1;
+                if(highscore <= 0) {
+                    clearInterval(highscoreTimer);
+        
+                    $("#highScore").html= $("#highScore").text("High Score: " + highscore);
+                } 
+            }, 1000
+                );
+
+            $("pressKey").html= $("#pressKey").text("Level " + level);
+        } 
+    }, 1000
+        );
+        
+//   nextSequence();
+
+    }
+ 
+   
+  });
+
+
+//SHOW QUESTION
+$(".btn").click(function(){
+
+checkAnswer(myQuestions);
+}
+)
+
+
+
+function checkAnswer(currentLevel) {
+    if(chosenAnswer[currentLevel] === myQuestions[currentLevel]) {
+        if(chosenAnswer.value = myQuestions[1, 2]) {
+            playSound("correct");
+            $("#pressKey").text("Congratulation");
+            setTimeout(function () {
+                $("body").removeClass("hide");
+              }, 200);
+        }else {
+            playSound("wrong")
+
+        }
+    }
+    
+}
+function playSound(name) {
+    var audio = new Audio("sound/" + name + ".mp3");
+    audio.play();
+  }
