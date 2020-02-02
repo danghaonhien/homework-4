98139023b952a04b7538379a4e4633ba8a78f8c1
danghaonhien <58311274+danghaonhien@users.noreply.github.com>
2020-02-01 19:22:21 -0800

    working on Saturday night

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ let level = 0;
 let countdown = 0;
 let nextButton = $("#nextQuestion");
 let answerValue = document.querySelector("#btn-answer");
-let shuffleQuestions , currentQuestionNum;
-$("#nextQuestion").on("click" , function() {
-    currentQuestionNum++;
-    nextQuestion();
-})
+let shuffleQuestions, currentQuestionNum;
+let ended = false;
+$("#pressKey").removeClass('hide2')
+$("#nextQuestion").on("click", function(event) {
+  event.preventDefault();
+  currentQuestionNum++;
+  nextQuestion();
+});
 $("#instruction").removeClass("hide2");
 let startGame = $(document).keypress(function() {
   if (!start) {
@@ -18,7 +21,6 @@ let startGame = $(document).keypress(function() {
 
     let timeLeft = 3;
     let countdownTimer = setInterval(function() {
-       
       $("pressKey").value = timeLeft - 1;
       $("#pressKey").html = $("#pressKey").text(
         timeLeft + " seconds remaining"
@@ -26,114 +28,116 @@ let startGame = $(document).keypress(function() {
       timeLeft -= 1;
       if (timeLeft <= 0) {
         clearInterval(countdownTimer);
-
+        $("#pressKey").addClass('hide2')
         // Shuffle questions
         shuffleQuestions = myQuestions.sort(() => Math.random() - 0.5);
-        currentQuestionNum=0;
+        currentQuestionNum = 0;
         // Show questions
         $("#question-list").removeClass("hide");
-        $("#instruction").addClass("hide2")
+        $("#instruction").addClass("hide2");
         //   nextQuestion();
-       nextQuestion();
+        nextQuestion();
         //High Score Timer
-         highscore = 100;
+        highscore = 100;
         let highscoreTimer = setInterval(function() {
           $("#highScore").value = highscore - 1;
-          $("#highScore").html = $("#highScore").text(
-            highscore + " seconds remaining"
-          );
-          highscore -= 1;
-          
+          // $("#highScore").html = $("#highScore").text(
+          //   highscore + " seconds remaining"
+          // );
+          highscore --;
+
           if (highscore <= 0) {
             clearInterval(highscoreTimer);
 
-            $("#highScore").html = $("#highScore").text(
-              "High Score: " + highscore
-            );
+            // $("#highScore").html = $("#highScore").text(
+            //   "High Score: " + highscore
+            // );
+          }else if (!ended) {
+           
+            
+            $("#yourScore").text("Your Highscore " + highscore)
+            
+            // clearInterval(highscoreTimer);
           }
         }, 1000);
 
-        $("pressKey").html = $("#pressKey").text("Level " + level);
+        $("#highScore").html = $("#pressKey").text("Level " + level);
       }
-    }, 1000); 
-  } 
- 
+    }, 1000);
+  }
 });
+
+$("#reset").on("click", function(){
+  ended=true
+  $("#question-list").addClass('hide')
+})
+// function yourScore(){
+//   let yourHighscore = highscoreTimer.value;
+//   $("#pressKey").html.$("#pressKey").text("Your Highscore is " + yourHighscore)
+
+// if(highscore.value === lastQuestion.value) {
+//   yourScore()
+// }}
 
 // NextQuestion
 function nextQuestion() {
   resetState();
   showQuestions(shuffleQuestions[currentQuestionNum]);
   level++;
-  $("pressKey").html = $("#pressKey").text("Level " + level);
+  $("#highScore").html = $("#highScore").text("Level " + level);
 }
 
-function showQuestions(question) {
-  $("#question").text(question.question);
-
-  question.answers.forEach(answers => {
-    const button = document.createElement('button');
-    button.innerText=answers.text;
-    button.classList.add("btn");
-    if (answers.correct) {
-      button.dataset.correct=answers.correct;
-    } else {
-        button.dataset.wrong=answers.wrong;
-    }
-    button.addEventListener("click", chosenAnswer) 
-    answerValue.appendChild(button);
-    
-  });
-}
 function resetState() {
-    clearState(document.body);
-    while(answerValue.firstChild) {
-      answerValue.removeChild(answerValue.firstChild);
-    }
+  clearState(document.body);
+  while (answerValue.firstChild) {
+    answerValue.removeChild(answerValue.firstChild);
   }
+}
 
 function chosenAnswer(e) {
   let chosenButton = e.target;
   let correct = chosenButton.dataset.correct;
-
- 
- 
-  $(this).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).css("backgroundColor", "#72A68E");
-    if (chosenButton.dataset.correct = correct) {
-      playSound("correct");
-      $("#pressKey").text("Congratulation");
-      $(".btn").on("click" , function() {
-        currentQuestionNum++;
-        nextQuestion();
-    })
-    } else{
-      playSound("wrong");
-      $("#pressKey").text("Try Again!");
-      $(this).css("backgroundColor", "#BF565F").css("color", "#F2F2F2");
-      highscore -=10;
-}
+  $(this)
+    .fadeOut(100)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100)
+    .css("backgroundColor", "#72A68E");
+  if ((chosenButton.dataset.correct = correct)) {
+    playSound("correct");
+    $("#pressKey").text("Congratulation");
+    $(".btn").on("click", function() {
+      currentQuestionNum++;
+      nextQuestion();
+    });
+  } else {
+    playSound("wrong");
+    $("#pressKey").text("Try Again!");
+    $(this)
+      .css("backgroundColor", "#BF565F")
+      .css("color", "#F2F2F2");
+    highscore -= 10;
+  }
   //set current state for answer
   currentState(document.body, correct);
   Array.from(answerValue.children).forEach(button => {
     currentState(button, button.dataset.correct);
-  
-
   });
-  if(shuffleQuestions.length > currentQuestionNum + 4) {
-  
-    $("#yourScore").html = $("#yourScore").text(" Your score is " + highscoreTimer) 
-  }
+
+  //   if(shuffleQuestions = lastQuestion  ) {
+  //       $("#question-list").addClass("hide")
+  //     $("#pressKey").html = $("#pressKey").text("Your Score is " + highscoreTimer)
+  //   }
 }
-
-
-
-
 
 function currentState(element, correct) {
   clearState(element);
-  if(correct) {
+  if (correct) {
     element.classList.add("correct");
-
   } else {
     element.classList.add("wrong");
   }
@@ -171,7 +175,7 @@ let myQuestions = [
       { text: "Rat", correct: true }
     ]
   },
-   {
+  {
     question: "Which one of these are the hardest trail in Zion National Park?",
     answers: [
       { text: "Angel Landings", correct: true },
@@ -179,8 +183,36 @@ let myQuestions = [
       { text: "Watchman Trail", correct: false },
       { text: "Pa'rus Trail", correct: false }
     ]
-   } 
+  }
 ];
+function showQuestions(question) {
+  $("#question").text(question.question);
+  // if (question.answers.length === lastQuestion) {
+  //   $("#question").text("Your score is: ")
+  // }
+  question.answers.forEach(answers => {
+    const button = document.createElement("button");
+    button.innerText = answers.text;
+    button.classList.add("btn");
+    if (answers.correct) {
+      button.dataset.correct = answers.correct;
+    }
+    button.addEventListener("click", chosenAnswer);
+    answerValue.append(button);
+  });
+}
+let lastQuestion = myQuestions[0].length;
+
+function startOver() {
+  level = 0;
+  myQuestions.question = [];
+  started = false;
+}
+$("#reset").on("click", function() {
+  startOver();
+  currentQuestionNum++;
+  $("#");
+});
 
 function playSound(name) {
   var audio = new Audio("sound/" + name + ".mp3");
